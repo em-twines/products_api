0ea264b84e69112ea63073737758a517f481c1d1
emilytwines <emdelbridge@gmail.com>
2022-11-10 15:18:56 -0500

    added paths to all products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Products
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def products_list(request):
+    products = Products.objects.all()
+    serializer = ProductSerializer(products, many=True)
+       
+    
+    return Response(serializer.data)
+
+
