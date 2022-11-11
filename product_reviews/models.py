9fe207ff8a2a4a6485ff90d509adea00154da945
emilytwines <emdelbridge@gmail.com>
2022-11-11 11:03:39 -0500

    migrated product_reviews

--- product_reviews/models.py
+++ product_reviews/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from products.models import Products
+
+
 # Create your models here.
 
 class Review(models.Model):
     review_text = models.CharField(max_length = 255)
     rating = models.IntegerField(validators = [MinValueValidator(1), MaxValueValidator(5)])
     name = models.CharField(max_length=255)
-    product = models.ForeignKey
+    product = models.ForeignKey(Products, on_delete = models.CASCADE)
 
-    
+
--- products_project/settings.py
+++ products_project/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'products'
+    'products',
+    'product_reviews'
 ]
 
 MIDDLEWARE = [
